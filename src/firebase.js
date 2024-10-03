e80b47625ae689eefc481f9cc2f8c7822e77c969
zion <ajz317@nyu.edu>
2024-10-03 18:22:40 +0600

    using .env file

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getStorage } from 'firebase/storage';
 
 // firebase
 const firebaseConfig = {
-  apiKey: "***REMOVED***",
-  authDomain: "***REMOVED***.firebaseapp.com",
-  projectId: "***REMOVED***",
-  storageBucket: "***REMOVED***",
-  messagingSenderId: "***REMOVED***",
-  appId: "***REMOVED***"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
